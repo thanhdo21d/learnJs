5706ddda62fbd1148c51ca353dfb0a7a29b06345
thanhdo21d <98605574+thanhdo21d@users.noreply.github.com>
2023-02-21 13:52:03 +0700

    add

---
+++ app.js
@@ -0,0 +1,10 @@
+const number = 100;
+console.log(number);
+let oderNumber =200;
+
+oderNumber = 500;
+console.log(oderNumber);
+//data style number string null undefined
+
+// const let khoong bi hoisting
+// var bi hoisting
